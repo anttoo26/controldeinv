86e1ba178717b399ecb6ebd96b3558f7ff9e6752
anttoo26 <madelatorre@tracsa.com.mx>
2024-09-10 10:48:59 -0600

    archivos

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.template')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="text-center">
+                    <h1>Welcome to Control de Inventario</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
